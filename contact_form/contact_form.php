e12491904839bf45ce985e8453aed2d5e1890dfd
Kevin <aseril@Kevins-Mac-mini.lan>
2020-05-24 21:38:18 -0600

    restructure projects on portfolio

--- contact_form/contact_form.php
+++ contact_form/contact_form.php
@@ -8,7 +8,6 @@ $fields = array('name' => 'Name', 'email' => 'Email', 'message' => 'Message'); /
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
 $errorMessage = 'There was an error while submitting the form. Please try again later';
 
-// let's do the sending
 
 if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])):
     //your site secret key
